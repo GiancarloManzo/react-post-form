a952db0f844f938fab5a08d1b5295cc16289f113
Giancarlo <cooper458.gm@gmail.com>
2026-02-17 03:42:58 +0100

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function App() {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [isPublic, setIsPublic] = useState(true);
+
   return (
     <div style={{ padding: 20 }}>
       <h1>Post Form</h1>
